61b1b59b5b330bbbc0d4f1bd7f52d642fb363794
고수희 <dev.suhee@gmail.com>
2024-02-11 13:47:05 +0900

    pass 5nd stage

--- app/main.js
+++ app/main.js
@@ -1,10 +1,24 @@
 const net = require('net');
 
+const PORT = 6379;
+const HOST = '127.0.0.1';
+
 const server = net.createServer((connection) => {
-  connection.on('data', () => {
-    console.log('data received');
-    connection.write('+PONG\r\n');
+  connection.on('data', (data) => {
+    const request = data.toString().split('\r\n');
+    const [command, message] = [request[2].toLowerCase(), request[4]];
+
+    switch (command) {
+      case 'echo':
+        connection.write(`+${message}\r\n`);
+        break;
+      case 'ping':
+        connection.write('+PONG\r\n');
+        break;
+    }
   });
 });
-//
-server.listen(6379, '127.0.0.1');
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server listening on ${HOST}:${PORT}`);
+});
