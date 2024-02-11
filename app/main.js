dc528dcddaa507d7ea70cfbaf4032e9471c59526
고수희 <dev.suhee@gmail.com>
2024-02-11 14:06:23 +0900

    pass 6nd stage

--- app/main.js
+++ app/main.js
@@ -3,17 +3,27 @@ const net = require('net');
 const PORT = 6379;
 const HOST = '127.0.0.1';
 
+let dataStore = new Map();
+
 const server = net.createServer((connection) => {
   connection.on('data', (data) => {
     const request = data.toString().split('\r\n');
-    const [command, message] = [request[2].toLowerCase(), request[4]];
+    const command = request[2].toLowerCase();
+    const [key, value] = [request[4], request[6]];
 
     switch (command) {
       case 'echo':
-        connection.write(`+${message}\r\n`);
+        connection.write(`+${key}\r\n`);
         break;
       case 'ping':
         connection.write('+PONG\r\n');
+        break;
+      case 'set':
+        dataStore.set(key, value);
+        connection.write('+OK\r\n');
+        break;
+      case 'get':
+        connection.write(dataStore?.get(key) ?? '(nil)\r\n');
         break;
     }
   });
