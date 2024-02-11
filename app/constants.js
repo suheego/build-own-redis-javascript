65fd5605cd0a0fbe9025e5d03aa96e8f3e834389
고수희 <dev.suhee@gmail.com>
2024-02-11 17:42:08 +0900

    add: new value types to constants.js

--- app/constants.js
+++ app/constants.js
@@ -11,3 +11,17 @@ export const OP_CODES = {
   RESIZE_DB: 0xfb,
   AUX: 0xfa,
 };
+
+export const VALUE_TYPE = {
+  STRING: 0,
+  LIST: 1,
+  SET: 2,
+  SORTED_ST: 3,
+  HASH: 4,
+  ZIP_MAP: 9,
+  ZIP_LIST: 10,
+  ZIP_SET: 11,
+  SORTED_SET_IN_ZIP_LIST: 12,
+  HASHMAP_IN_ZIP_LIST: 13,
+  LIST_QUICKLIST: 14,
+};
